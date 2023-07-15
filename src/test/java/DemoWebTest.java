91bcabeef1c0e2d07efcc0f8c6577bac4b489586
doti0501 <114552042+doti0501@users.noreply.github.com>
2023-07-15 20:12:03 +0700

    Update DemoWebTest.java

--- src/test/java/DemoWebTest.java
+++ src/test/java/DemoWebTest.java
@@ -1,20 +1,24 @@
 
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
+import org.junit.*;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import javax.swing.*;
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 
 public class DemoWebTest {
 	WebDriver driver;
 
-	@BeforeClass
+	@Before
 	public void setup(){
 		WebDriverManager.chromedriver().setup();
 		driver = new ChromeDriver();
@@ -24,19 +28,24 @@ public class DemoWebTest {
 	@Test
 	public void testCase1() {
 		driver.getTitle();
-		WebElement Dbutton = driver.findElement(By.linkText("Learn more"));
+		//WebElement Learn_more_btn = driver.findElement(By.linkText("Learn more"));
 		//Action act = new Actions(driver);
-		Dbutton.click();
+		//Learn_more_btn.click();
+
+
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		WebElement element = wait.until(ExpectedConditions.elementToBeClickable(By.linkText("Learn more")));
+		((JavascriptExecutor)driver).executeScript("arguments[0].click();", element);
 
 	}
-	@AfterClass
+	@After
 	public void tearDown() {
-		driver.close();
+		driver.quit();
 	}
 
 
 //	public static void main (String[] args) {
-//		System.setProperty("Webdriver.chrome.driver","D:\\chromedriver.exe");
+//		System.setProperty("WebDriver.chrome.driver","D:\\chromedriver.exe");
 //		WebDriverManager.chromedriver().setup();
 //		WebDriver driver = new ChromeDriver();
 //		driver.manage().window().maximize();
@@ -44,4 +53,4 @@ public class DemoWebTest {
 
 	}
 
-}
+
