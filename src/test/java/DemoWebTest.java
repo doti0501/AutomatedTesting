6e90fc3d178badac296b70599c5b17b35d334759
Huong Nguyen Thi <huong.nguyenthi@aswhiteglobal.com>
2023-07-15 08:35:15 +0700

    add src

---
+++ src/test/java/DemoWebTest.java
@@ -0,0 +1,19 @@
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class DemoWebTest {
+	public static void main (String[] args) {
+		//System.setProperty("Webdriver.chrome.driver","D:\\chromedriver.exe");
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://dev.smartskip.com.au");
+		System.out.println(driver.getTitle());
+		System.out.println(driver.getCurrentUrl());
+		driver.close();
+	}
+
+}
